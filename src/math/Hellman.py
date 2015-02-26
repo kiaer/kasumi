be7db97a6eca8b67d8705d2366eaeecf42db9d2b
Becktor <JonathanBecktor@gmail.com>
2015-02-26 17:59:23 +0100

    [CHORE] rm wrong folder and heman

---
+++ src/math/Hellman.py
@@ -0,0 +1,7 @@
+Local:    master ~/Git/kasumi/
+Remote:   master @ origin (https://github.com/kiaer/kasumi.git)
+Head:     5b08011 Hellman sage implemen script etellerande
+
+Stashes:
+0: WIP on master: 3fec2f5 initial commit. contains kasumi cipher.
+
