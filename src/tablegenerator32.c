850fe8cfa9ab9c8bdd6f7cbc4226b78c91e25849
Christian Kiær <ckiaer@hotmail.com>
2015-04-22 18:27:29 +0200

    32bit generator should be working

--- src/tablegenerator32.c
+++ src/tablegenerator32.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <pthread.h>
+#include <time.h>
 
 #include "cipher/kasumi.c"
 
 uint16_t * keyGen(){
     int i;
-    int byte_count = 8;
+    int byte_count = 4;
     static uint16_t data[8];
     FILE *fp;
     fp = fopen("/dev/urandom", "r");
     fread(&data, 1, byte_count, fp);
     fclose(fp);
-    for (i = 6; i < 8; i++){
-        data[i] = data[(i + 2) % 8];
+    for (i = 2; i < 8; i++){
+        data[i] = data[(i + 6) % 8];
     }
     return data;
 }
@@ -25,7 +26,7 @@ void tableGenerator(uint32_t * text){
     uint16_t key[8], ep[2];
     FILE *write_ptr;
     write_ptr = fopen("test32.bin","wb");
-     for(m = 0; m < 5; m++){
+    for(m = 0; m < 33554432; m++){
         temp = keyGen();
         /* for (i = 0; i < 4; i++){ */
         /*     sp[i] = temp[i];
@@ -35,26 +36,30 @@ void tableGenerator(uint32_t * text){
         }
 
         /* printf("\n 0x "); */
-        /* for (i = 0; i < 4; i++) */
-        /*     printf(" %04x ", sp[i]); */
-        for (t = 0; t < 10; t++){
+        /* for (i = 0; i < 8; i++) */
+        /*     printf(" %04x ", key[i]); */
+        for (t = 0; t < 236; t++){
             keyschedule(key);
             temp = kasumi_enc(text);
             for (i = 0; i < 8; i++){
                 key[i] = temp[i % 2];
             }
+            /* printf("\n 0x "); */
+            /* for (i = 0; i < 8; i++) */
+            /*     printf(" %04x ", key[i]); */
         }
 
         for (i = 0; i < 2; i++){
             ep[i] = key[i];
         }
-        printf("\n 0x ");
-        for (i = 0; i < 2; i++)
-            printf(" %04x ", ep[i]);
+        
+        /* printf("\n 0x "); */
+        /* for (i = 0; i < 2; i++) */
+        /*     printf(" %04x ", ep[i]); */
 
         fwrite(ep,sizeof(ep),1,write_ptr);
     }
-     fclose(write_ptr);
+    fclose(write_ptr);
 
     /* int i; */
     /* uint16_t * key2 = keyGen(); */
@@ -68,9 +73,9 @@ int main(){
     /* uint16_t key[4] = { */
     /*     0x9900, 0xAABB, 0xCCDD, 0xEEFF */
     /* }; */
-    int amountOfKeys=5;
-    uint16_t buffer[amountOfKeys*4];
-    FILE *ptr;
+    /* int amountOfKeys=5; */
+    /* uint16_t buffer[amountOfKeys*4]; */
+    /* FILE *ptr; */
 
 
     uint32_t text[2] = {
@@ -79,19 +84,32 @@ int main(){
 
 
     tableGenerator(text);
-    ptr = fopen("test32.bin","rb");  // r for read, b for binary
+    /* ptr = fopen("test32.bin","rb");  // r for read, b for binary */
 
-    fread(buffer,sizeof(buffer),1,ptr); // read 10 bytes to our buffer
-    int i,j,cnt,cnt1;
-    printf("\n Read \n");
-    for(j = 0; j<amountOfKeys ; j++){
-        printf(" 0x ");
-        cnt=j*2;
-         for(i = 0; i<2; i++){
-             cnt1=cnt+i;
-             printf(" %04x ", buffer[cnt1]);
-         } // prints a series of bytes}
-        printf("\n");
-    }
+    /* fread(buffer,sizeof(buffer),1,ptr); // read 10 bytes to our buffer */
+    /* int i,j,cnt,cnt1; */
+    /* printf("\n Read \n"); */
+    /* for(j = 0; j<amountOfKeys ; j++){ */
+    /*     printf(" 0x "); */
+    /*     cnt=j*2; */
+    /*      for(i = 0; i<2; i++){ */
+    /*          cnt1=cnt+i; */
+    /*          printf(" %04x ", buffer[cnt1]); */
+    /*      } // prints a series of bytes} */
+    /*     printf("\n"); */
+    /* } */
     return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
