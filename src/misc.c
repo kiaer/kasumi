e49f9cf7ff785cfb06110cb073b3d83ad858c0b9
Christian Kiær <ckiaer@hotmail.com>
2015-06-03 01:40:49 +0200

    new misc file for generic functions used across code

---
+++ src/misc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <time.h>
+#include <openssl/md5.h>
+#include <stdlib.h>
+
+#include "cipher/kasumi.h"
+
+
+uint16_t * keyGen(int m){
+    int j,i, arrToInt,cntr=0;
+    static uint16_t data[8];
+    unsigned char c[MD5_DIGEST_LENGTH];
+    MD5_CTX mdContext;
+    MD5_Init (&mdContext);
+    MD5_Update (&mdContext,&m, sizeof(m));
+    MD5_Final (c,&mdContext);
+    for (i = 0; i < 8; i++){
+        arrToInt=0;
+        for(j=cntr;j<=cntr+1;j++)
+            arrToInt =(arrToInt<<8) | c[j%4];
+        data[i] = arrToInt;
+        cntr=cntr+2;
+    }
+    return data;
+}
---
+++ src/misc.h
@@ -0,0 +1,6 @@
+#ifndef __MISC_H__
+#define __MISC_H__
+
+uint16_t * keyGen(int m);
+
+#endif //MISC_H
